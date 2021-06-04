fc61734bc3baf9fb703e94edf6c63cd2fee64a8c
dawn <congminh292k@gmail.com>
2021-06-04 19:52:28 +0700

    little fix

--- src/game/entities/creatures/Player.java
+++ src/game/entities/creatures/Player.java
@@ -109,8 +109,7 @@ public class Player extends Creature {
 	}
 
 	public void die() {
-		handler.getWorld().getEntityManager().getNewEntityQueue().add(new Tomb(handler, x, y));
-		System.out.println(handler.getGame().getGameInfo().getScore());
+//		handler.getWorld().getEntityManager().getNewEntityQueue().add(new Tomb(handler, x, y));
 		handler.getWorld().getEntityManager().setPlayerIsDead(true);
 	}
 }
