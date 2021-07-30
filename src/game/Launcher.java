a658c9d6520a3e20e3a142ecf283ed1c95cda3ed
Minh <congminh292k@gmail.com>
2021-07-30 10:22:14 +0700

    Update Launcher.java

--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -2,7 +2,7 @@ package game;
 
 public class Launcher {
 	public static void main(String[] args) {
-		Game game = new Game("Simple RPG", 1366, 768);
+		Game game = new Game("Dino RPG", 1366, 768);
 		game.start();
 	}
 }
